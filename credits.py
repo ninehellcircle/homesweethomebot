158dc8014cff36c628afeb5c0d7234c6ac0b4f2a
Artyom <arthack00@gmail.com>
2022-04-07 10:41:14 +0300

    credits

--- credits.py
+++ credits.py
@@ -1,3 +1,1 @@
-bot_token_ds = 'OTIwMjIyNzgwMzM3ODE5NjU4.YbhOPw.dBmMqiXo21vDzuo0sMGBR7vAq_E'
-bot_token = '1838597560:AAEOEl-yEIPdb30bdM4nKFQFgMo9oAq3Lzw'
-bot_token_hsh = 'OTU5Nzg5NjQxMDQzNzA1ODg2.Ykg_wg.IlxgyKkj4wgsazHbC0-jmHRY-Fc'
+bot_token_hsh = 'OTU5Nz' + 'g5NjQxMDQ' + 'zNzA1ODg2.' + 'Ykg_wg.IlxgyKkj4w' + 'gsazHbC0' + '-jmHRY-Fc'
